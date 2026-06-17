4fc7275c829fd4f31325f96aac4dd94366e7d062
BOPITHARAN <140912823+BOPITHARAN@users.noreply.github.com>
2026-06-17 18:25:27 +0530

    Update CORS configuration to include Netlify URL

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "http://localhost:5175",
+      "https://jobcente.netlify.app",
     ],
     credentials: true,
   })
